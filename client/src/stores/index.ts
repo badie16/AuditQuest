656d9d9462cc98921eb3d815033917ee081f33a1
badie16 <badi3bahida16@gmail.com>
2026-03-12 01:40:21 +0000

    feat: add dialogue reducer to the store for managing dialogue state

--- client/src/stores/index.ts
+++ client/src/stores/index.ts
@@ -6,6 +6,7 @@ import whiteboardReducer from './WhiteboardStore'
 import chatReducer from './ChatStore'
 import roomReducer from './RoomStore'
 import auditReducer from './AuditStore'
+import dialogueReducer from './DialogueStore'
 
 enableMapSet()
 
@@ -17,6 +18,7 @@ const store = configureStore({
     chat: chatReducer,
     room: roomReducer,
     audit: auditReducer,
+    dialogue: dialogueReducer,
   },
   // Temporary disable serialize check for redux as we store MediaStream in ComputerStore.
   // https://stackoverflow.com/a/63244831
